b191bae60cffcccf9de96f201d19c9b48e46072c
Radusek <Radusek@github.com>
2020-05-06 17:53:25 +0200

    Preparation

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.forward, rotationSpeed);
+    }
+}
