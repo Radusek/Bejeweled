c2312b66f73a39d1390560b1b6251c682cddf8e2
Radosław Rychlik <radusek4@gmail.com>
2020-05-09 01:53:07 +0200

    Basic jewel swapping functionality introduced

--- Assets/Scripts/Jewel.cs
+++ Assets/Scripts/Jewel.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public enum JewelType
 {
@@ -11,17 +12,35 @@ public enum JewelType
 
 public class Jewel : MonoBehaviour
 {
+    [SerializeField]
+    private Color[] jewelColors;
+
+    public JewelGrid Grid { get; set; }
     public Vector2Int GridIndex { get; set; }
     public JewelType Type { get; private set; }
 
+    
 
     private void Awake()
     {
         Type = (JewelType)Random.Range(0, (int)JewelType.Count);
+        GetComponent<Image>().color = jewelColors[(int)Type];
     }
 
     public void OnClick()
     {
+        Grid.JewelClicked(GridIndex);
+
         Debug.Log($"Clicked on {Type} at {GridIndex}");
+    }
+
+    public void OnMouseButtonRelease()
+    {
+        Grid.MouseButtonReleased();
+    }
+
+    public void OnMousePointerEnter()
+    {
+        Grid.JewelPointerEnter(GridIndex);
     }
 }
--- Assets/Scripts/JewelGrid.cs
+++ Assets/Scripts/JewelGrid.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using UnityEngine.UI;
+﻿using DG.Tweening;
+using UnityEngine;
 
 public class JewelGrid : MonoBehaviour
 {
@@ -11,6 +11,14 @@ public class JewelGrid : MonoBehaviour
 
     private Jewel[,] jewelGrid;
 
+    private int swappingAnimations;
+
+    private bool mouseButtonHeld;
+    private Vector2Int clickedJewelIndex;
+
+    [SerializeField]
+    private float swappingTime = 0.5f;
+
 
     private void Awake()
     {
@@ -20,15 +28,67 @@ public class JewelGrid : MonoBehaviour
         {
             for (int j = 0; j < gridSize.x; j++)
             {
-                GameObject newJewel = Instantiate(jewelPrefab, transform);
-                Jewel jewelScript = newJewel.GetComponent<Jewel>();
-
-                jewelScript.GridIndex = new Vector2Int(i, j);
-                jewelGrid[i, j] = jewelScript;
+                CreateJewel(i, j);
             }
         }
+    }
 
-        jewelGrid[2, 0].GetComponent<Image>().color = Color.black;
-        jewelGrid[3, 5].GetComponent<Image>().color = Color.yellow;
+    private void CreateJewel(int i, int j)
+    {
+        GameObject newJewel = Instantiate(jewelPrefab, transform);
+        Jewel jewelScript = newJewel.GetComponent<Jewel>();
+
+        jewelScript.Grid = this;
+        jewelScript.GridIndex = new Vector2Int(i, j);
+        jewelGrid[i, j] = jewelScript;
+    }
+
+    public void JewelClicked(Vector2Int index)
+    {
+        mouseButtonHeld = true;
+        clickedJewelIndex = index;
+    }
+
+    public void MouseButtonReleased()
+    {
+        mouseButtonHeld = false;
+    }
+
+    public void JewelPointerEnter(Vector2Int index)
+    {
+        if (!mouseButtonHeld || swappingAnimations > 0)
+            return;
+
+        int vectorsDifference = 0;
+        vectorsDifference += Mathf.Abs(index.x - clickedJewelIndex.x);
+        vectorsDifference += Mathf.Abs(index.y - clickedJewelIndex.y);
+
+        if (vectorsDifference == 1)
+            SwapJewels(index, clickedJewelIndex);
+
+        mouseButtonHeld = false;
+    }
+
+    public void SwapJewels(Vector2Int indexA, Vector2Int indexB)
+    {
+        Jewel jewelA = jewelGrid[indexA.x, indexA.y];
+        Jewel jewelB = jewelGrid[indexB.x, indexB.y];
+
+        swappingAnimations = 2;
+
+        jewelA.transform.DOLocalMove(jewelB.transform.localPosition, swappingTime).OnComplete(OnSwappingAnimationCompleted);
+        jewelB.transform.DOLocalMove(jewelA.transform.localPosition, swappingTime).OnComplete(OnSwappingAnimationCompleted);
+
+        Vector2Int jewelAIndex = jewelA.GridIndex;
+        jewelA.GridIndex = jewelB.GridIndex;
+        jewelB.GridIndex = jewelAIndex;
+
+        jewelGrid[indexA.x, indexA.y] = jewelB;
+        jewelGrid[indexB.x, indexB.y] = jewelA;
+    }
+
+    private void OnSwappingAnimationCompleted()
+    {
+        swappingAnimations--;
     }
 }
