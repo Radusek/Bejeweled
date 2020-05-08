ff9891b4883dd9bd7f734bd2e986a9cce459cd84
Radusek <Radusek@github.com>
2020-05-08 12:47:58 +0200

    JewelGrid and Jewel class added

---
+++ Assets/Scripts/Jewel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public enum JewelType
+{
+    Emerald,
+    Sapphire,
+    Diamond,
+    Ruby,
+    Count //często dodaję Count na koniec enuma żeby móc uzyskać liczbę elementów tworzących enuma
+}
+
+public class Jewel : MonoBehaviour
+{
+    public Vector2Int GridIndex { get; set; }
+    public JewelType Type { get; private set; }
+
+
+    private void Awake()
+    {
+        Type = (JewelType)Random.Range(0, (int)JewelType.Count);
+    }
+
+    public void OnClick()
+    {
+        Debug.Log($"Clicked on {Type} at {GridIndex}");
+    }
+}
---
+++ Assets/Scripts/JewelGrid.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class JewelGrid : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject jewelPrefab;
+
+    [SerializeField]
+    private Vector2Int gridSize;
+
+    private Jewel[,] jewelGrid;
+
+
+    private void Awake()
+    {
+        jewelGrid = new Jewel[gridSize.x, gridSize.y];
+
+        for (int i = 0; i < gridSize.y; i++)
+        {
+            for (int j = 0; j < gridSize.x; j++)
+            {
+                GameObject newJewel = Instantiate(jewelPrefab, transform);
+                Jewel jewelScript = newJewel.GetComponent<Jewel>();
+
+                jewelScript.GridIndex = new Vector2Int(i, j);
+                jewelGrid[i, j] = jewelScript;
+            }
+        }
+
+        jewelGrid[2, 0].GetComponent<Image>().color = Color.black;
+        jewelGrid[3, 5].GetComponent<Image>().color = Color.yellow;
+    }
+}
